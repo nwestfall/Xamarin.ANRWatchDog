bc5e8d469d7563138801bb5a34000424f3861c82
Nathan Westfall <nathan.westfall@tylertech.com>
2017-10-11 15:39:14 -0400

    initial commit

---
+++ ANRWatchDog/ANRError.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Android.OS;
+
+using Java.Lang;
+
+namespace Xamarin.ANRWatchDog
+{
+    /// <summary>
+    /// Error thrown by <see cref="ANRWatchDog"/> when an ANR is detected.
+    /// Contains the stack trace of the frozen UI thread.
+    /// 
+    /// It is important to notice that, in an ANRError, all the "Caused by" are not really the cause
+    /// of the exception.  Each "Caused by" is the stack trace of the running thread.  Note that the main
+    /// thread always comes first.
+    /// </summary>
+    [SuppressWarnings(Value = new string[] { "Convert2Diamond", "UnusedDeclaration" })]
+    public class ANRError : Error
+    {
+        [Serializable]
+        private class _ThreadTrace
+        {
+            public static string Name { get; private set; }
+            public static StackTraceElement[] StackTrace { get; private set; }
+
+            internal class _Thread : Throwable
+            {
+                internal _Thread(_Thread other) : base(Name, other) { }
+
+                public override Throwable FillInStackTrace()
+                {
+                    SetStackTrace(_ThreadTrace.StackTrace);
+                    return this;
+                }
+            }
+
+            internal _ThreadTrace(string name, StackTraceElement[] stackTrace)
+            {
+                Name = name;
+                StackTrace = stackTrace;
+            }
+        }
+
+        private const long SERIAL_VERSION_UID = 1L;
+
+        private ANRError(_ThreadTrace._Thread st) : base("Application Not Responding", st) { }
+
+        public override Throwable FillInStackTrace()
+        {
+            SetStackTrace(new StackTraceElement[] { });
+            return this;
+        }
+
+        public static ANRError New(string prefix, bool logThreadsWithoutStackTrace)
+        {
+            var mainThread = Looper.MainLooper.Thread;
+
+            var threadComparer = new _StackTraceComparer(mainThread);
+
+            var stackTraces = new Dictionary<Thread, StackTraceElement[]>(threadComparer);
+
+            foreach(var entry in Thread.AllStackTraces)
+            {
+                if (entry.Key == mainThread || (entry.Key.Name.StartsWith(prefix) && (logThreadsWithoutStackTrace || entry.Value.Length > 0)))
+                    stackTraces.Add(entry.Key, entry.Value);
+            }
+
+            // Sometimes main is not returned in Thread.AllStackTraces - ensure that we list it
+            if (!stackTraces.ContainsKey(mainThread))
+                stackTraces.Add(mainThread, mainThread.GetStackTrace());
+
+            _ThreadTrace._Thread tst = null;
+            foreach (var entry in stackTraces)
+            {
+                var tt = new _ThreadTrace(GetThreadTitle(entry.Key), entry.Value);
+                tst = new _ThreadTrace._Thread(tst);
+            }
+
+            return new ANRError(tst);
+        }
+
+        public static ANRError NewMainOnly()
+        {
+            var mainThread = Looper.MainLooper.Thread;
+            var mainStackTrace = mainThread.GetStackTrace();
+
+            var tt = new _ThreadTrace(GetThreadTitle(mainThread), mainStackTrace);
+            var tst = new _ThreadTrace._Thread(null);
+
+            return new ANRError(tst);
+        }
+
+        private static string GetThreadTitle(Thread thread) => $"{thread.Name} (state = {thread.GetState()})";
+
+        private class _StackTraceComparer : IEqualityComparer<Thread>
+        {
+            private readonly Thread _mainThread;
+
+            public _StackTraceComparer(Thread mainThread) => _mainThread = mainThread;
+
+            public bool Equals(Thread lhs, Thread rhs)
+            {
+                if (lhs == rhs)
+                    return true;
+                if (lhs == _mainThread)
+                    return false;
+                if (rhs == _mainThread)
+                    return false;
+                return rhs.Name.Equals(lhs.Name);
+            }
+
+            public int GetHashCode(Thread obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+    }
+}
---
+++ ANRWatchDog/ANRWatchDog.cs
@@ -0,0 +1,172 @@
+﻿using Android.OS;
+using Android.Util;
+
+using Java.Lang;
+
+namespace Xamarin.ANRWatchDog
+{
+    /// <summary>
+    /// A watchdog timer thread that detects when the UI thread has frozen
+    /// </summary>
+    [SuppressWarnings(Value = new string[] { "UnusedDeclaration" })]
+    public class ANRWatchDog : Thread, IRunnable
+    {
+        public interface IANRListener
+        {
+            void OnAppNotResponding(ANRError error);
+        }
+
+        public interface IInterruptionListener
+        {
+            void OnInterrupted(InterruptedException e);
+        }
+
+        private const int DEFAULT_ANR_TIMEOUT = 5000;
+
+        private class DefaultANRListener : IANRListener
+        {
+            public void OnAppNotResponding(ANRError error) => throw error;
+        }
+
+        private class DefaultInterruptionListener : IInterruptionListener
+        {
+            public void OnInterrupted(InterruptedException e) => Log.Warn("ANRWatchdog", $"Interrupted: {e.Message}");
+        }
+
+        private IANRListener _anrListener = new DefaultANRListener();
+        private IInterruptionListener _interruptionListener = new DefaultInterruptionListener();
+
+        private readonly Handler _uiHandler = new Handler(Looper.MainLooper);
+        private readonly int _timeoutInterval;
+
+        private string _namePrefix = "";
+        private bool _logThreadsWithoutStackTrace = false;
+        private bool _ignoreDebugger = false;
+
+        private static volatile int _tick = 0;
+
+        /// <summary>
+        /// Constructs a watchdog that checks the UI thread every <see cref="DEFAULT_ANR_TIMEOUT"/> milliseconds
+        /// </summary>
+        public ANRWatchDog() : this(DEFAULT_ANR_TIMEOUT) { }
+
+        /// <summary>
+        /// Constructs a watchdog that checks the ui thread every given interval
+        /// </summary>
+        /// <param name="timeoutInterval">The interval in milliseconds, between to checks of the UI thread.  It is therefore the maximum time the UI may freeze before being reported an ANR</param>
+        public ANRWatchDog(int timeoutInterval) : base() => _timeoutInterval = timeoutInterval;
+
+        /// <summary>
+        /// Sets an interface for when an ANR is detected
+        /// </summary>
+        /// <param name="listener">If not set, the default behavior is to throw an error and crash the application</param>
+        /// <returns>itself for chaining</returns>
+        public ANRWatchDog SetANRListener(IANRListener listener)
+        {
+            _anrListener = (listener == null) ? new DefaultANRListener() : listener;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets an interface for when the watchdog thread is interrupted.
+        /// If not set, the default behavior is to just log the interruption message
+        /// </summary>
+        /// <param name="listener">The new listener or null</param>
+        /// <returns>itself for chaining</returns>
+        public ANRWatchDog SetInterruptionListener(IInterruptionListener listener)
+        {
+            _interruptionListener = (listener == null) ? new DefaultInterruptionListener() : listener;
+            return this;
+        }
+
+        /// <summary>
+        /// Set thre prefix that a thread's name must have for the thread to be reported
+        /// Not that the main thread is always reported
+        /// Default "".
+        /// </summary>
+        /// <param name="prefix">The thread name's prefix for a thread to be reported</param>
+        /// <returns>itself for chaining</returns>
+        public ANRWatchDog SetReportThreadNamePrefix(string prefix)
+        {
+            _namePrefix = (prefix == null) ? string.Empty : prefix;
+            return this;
+        }
+
+        /// <summary>
+        /// Set that only the main thread will be reported
+        /// </summary>
+        /// <returns>itself for chaining</returns>
+        public ANRWatchDog SetReportMainThreadOnly()
+        {
+            _namePrefix = null;
+            return this;
+        }
+
+        /// <summary>
+        /// Set that all running threads will be reported,
+        /// even those from which no stack trace could be extracted
+        /// Default false
+        /// </summary>
+        /// <param name="logThreadsWithoutStackTrace">Whether or not all running threads should be reported</param>
+        /// <returns>itself for chaining</returns>
+        public ANRWatchDog SetLogThreadsWithoutStackTrace(bool logThreadsWithoutStackTrace)
+        {
+            _logThreadsWithoutStackTrace = logThreadsWithoutStackTrace;
+            return this;
+        }
+
+        /// <summary>
+        /// Set whether to ignore the debugger when detecting ANRs
+        /// When ingoring the debugger, ANRWatchdog will detect ANRs even if the debugger is connected.
+        /// By default, it does not, to avoid interpreting debugging pauses as ANRs.
+        /// Default false.
+        /// </summary>
+        /// <param name="ignoreDebugger">Whenter to ignore the debugger</param>
+        /// <returns>itself for chaining</returns>
+        public ANRWatchDog SetIgnoreDebugger(bool ignoreDebugger)
+        {
+            _ignoreDebugger = ignoreDebugger;
+            return this;
+        }
+
+        public override void Run()
+        {
+            Name = "|ANR-WatchDog|";
+
+            int lastTick, lastIgnored = -1;
+            while(!IsInterrupted)
+            {
+                lastTick = _tick;
+                _uiHandler.Post(() =>
+                {
+                    _tick = (_tick + 1) % Integer.MaxValue;
+                });
+                try
+                {
+                    Thread.Sleep(_timeoutInterval);
+                }
+                catch(InterruptedException e)
+                {
+                    _interruptionListener.OnInterrupted(e);
+                }
+
+                //If the main thread has not handled _ticker, it is blocked. ANR
+                if(_tick == lastTick)
+                {
+                    if(!_ignoreDebugger && Debug.IsDebuggerConnected)
+                    {
+                        if (_tick != lastIgnored)
+                            Log.Warn("ANRWatchdog", "An ANR was detected but ignored because the debugger is connected (you can prevent this with setIgnoreDebugger(true))");
+                        lastIgnored = _tick;
+                        continue;
+                    }
+
+                    ANRError error = (_namePrefix != null) ? ANRError.New(_namePrefix, _logThreadsWithoutStackTrace) : ANRError.NewMainOnly();
+                    _anrListener.OnAppNotResponding(error);
+                    return;
+
+                }
+            }
+        }
+    }
+}
